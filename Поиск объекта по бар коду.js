b1521bfd766da09c26f2d73c3ab5c9d68e7705bb
Lex1065 <165472209+Lex1065@users.noreply.github.com>
2024-03-30 16:06:55 +0400

    Update Поиск объекта по бар коду.js

--- Поиск объекта по бар коду.js
+++ Поиск объекта по бар коду.js
@@ -18,7 +18,7 @@
         }
         .countLabel {
             display: block;
-            color: green !important;
+            color: grey !important;
             margin-bottom: 0px;
             font-weight: bold !important;
         }
